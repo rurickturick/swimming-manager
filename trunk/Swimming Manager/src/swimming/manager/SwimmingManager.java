ad6f7ab3b17e9910f1bcf2edeaae70fc4ba9e588
juanluis411 <juanluis411@76616d90-0026-1082-0bbe-f8b8df388cbd>
2012-03-09 16:55:52 +0000

    Primer prototipo

---
+++ trunk/Swimming Manager/src/swimming/manager/SwimmingManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swimming.manager;
+
+/**
+ *
+ * @author Juanlu
+ */
+public class SwimmingManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow window = new MainWindow();
+    }
+}
