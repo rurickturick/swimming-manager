e663bfb9ae87242f6f68243c5ace71de795eb0f1
lara.carrion <lara.carrion@76616d90-0026-1082-0bbe-f8b8df388cbd>
2012-04-23 11:34:34 +0000

    Clase Nadador : hemos cambiado los arrays por arraylists y la hemos terminado Clase Marca: terminada

--- trunk/Swimming Manager/src/swimming/manager/Marca.java
+++ trunk/Swimming Manager/src/swimming/manager/Marca.java
@@ -1,15 +1,4 @@
 package swimming.manager;
-
-/**
- * 
- */
-
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author usuario_local
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class Marca {
 	
 	private Fecha fecha;
