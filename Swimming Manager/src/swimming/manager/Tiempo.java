c05dc02a6d8c32a098a5a3caa743605aa5426e2b
angelluisortiz1990 <angelluisortiz1990@76616d90-0026-1082-0bbe-f8b8df388cbd>
2012-05-14 12:14:21 +0000

    cambiado el formato de la clase tiempo
    M:S:mS

--- Swimming Manager/src/swimming/manager/Tiempo.java
+++ Swimming Manager/src/swimming/manager/Tiempo.java
@@ -2,22 +2,19 @@ package swimming.manager;
 
 public class Tiempo {
 	
-	private int horas;
+	
 	
 	private int minutos;
 	
 	private int segundos;
 	
-	private int decimas;
-	
-	private int centesimas;
+        private int milesimas;
 
 	public Tiempo() {
-            this.horas=0;
+           
             this.minutos=0;
-            this.segundos=00;
-            this.decimas=0;
-            this.centesimas=0;
+            this.segundos=0;
+            this.milesimas=0;
 	}
 
 	/** 
@@ -31,13 +28,11 @@ public class Tiempo {
 	 * @param intcentesimas
 	 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public Tiempo(int inthoras, int minutos,
-			int intsegudos, int intdecimas, int intcentesimas) {
-            this.horas=inthoras;
+	public Tiempo( int minutos,int segudos, int milesimas) {
+           
             this.minutos=minutos;
-            this.segundos=intsegudos;
-            this.decimas=intdecimas;
-            this.centesimas=intcentesimas;
+            this.segundos=segudos;
+            this.milesimas=milesimas;
 	}
 
 	/** 
@@ -51,22 +46,15 @@ public class Tiempo {
 	 * @param intcentesimas
 	 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public void setTiempo(int inthoras, int minutos,
-			int intsegudos, int intdecimas, int intcentesimas) {
-            this.horas=inthoras;
+	public void setTiempo( int minutos, int intsegudos, int milesimas) {
+          
             this.minutos=minutos;
             this.segundos=intsegudos;
-            this.decimas=intdecimas;
-            this.centesimas=intcentesimas;
+           this.milesimas=milesimas;
 	}
         
-        public int getHoras(){
-            return this.horas;
-        }
-        
-        public void setHoras(int horas){
-            this.horas = horas;
-        }
+       
+      
         
         public int getMinutos(){
             return this.minutos;
@@ -84,57 +72,43 @@ public class Tiempo {
             this.segundos = segundos;
         }
         
-        public int getDecimas(){
-            return this.decimas;
-        }
-        
-        public void setDecimas(int decimas){
-            this.decimas = decimas;
-        }
-        
-        public int getCentesimas(){
-            return this.centesimas;
-        }
-        
-        public void setCentesimas(int centesimas){
-            this.centesimas = centesimas;
-        }
+       
         
         
-         // devuelve true si a es mayor que b
-         //          false si b es mayor o igual a a
+         // devuelve true si a es menor que b
+         //          false si b es menor o igual a a
         public boolean compara(Tiempo a){
-            if (a.getHoras() > this.getHoras())
-                return true;
-            if (a.getMinutos() > this.getMinutos())
+            
+            /*if (a.getMinutos() > this.getMinutos())
                 return true;
             if (a.getSegundos() > this.getSegundos())
                 return true;
-            if (a.getDecimas() > this.getDecimas())
-                return true;
-            if (a.getCentesimas() > this.getCentesimas())
-                return true;
-            return false;
+            
+            return false;*/
+            if (a.minutos < this.minutos) return true;
+            else if (a.minutos == this.minutos){
+                if (a.segundos < this.segundos) return true;
+                else if (a.segundos == this.segundos){
+                    if (a.milesimas <= this.milesimas) return true;
+                    else return false;
+                }else return false;
+            }else return false;
         }
         
         public String saveToString(){
-            String s=""+this.horas+" "+
-                        this.minutos+" "+
+            String s= this.minutos+" "+
                         this.segundos+" "+
-                        this.decimas+" "+
-                        this.centesimas;
+                    this.milesimas;
             return s;
         }
         
 
          public String toString() {
-            String s1= ""+this.horas;
-            String s2= ""+this.minutos;
-            String s3= ""+this.segundos;
-            String s4= ""+this.decimas;
-            String s5= ""+this.centesimas;
+            String s1= ""+this.minutos;
+            String s2= ""+this.segundos;
+            String s3=""+this.milesimas;       
             
-            String resultado = s1 + ":" + s2 + ":" + s3 + ":" + s4 + ":" + s5;
+            String resultado = s1 + ":" + s2 + ":" + s3;
             return resultado;
 	}
 }
